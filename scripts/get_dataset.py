cad024dde4f8c764a542eb48827cec097b1d7255
iamPMV259 <minhvu2592005@gmail.com>
2025-03-20 15:14:00 +0700

    get data complete

--- scripts/get_dataset.py
+++ scripts/get_dataset.py
@@ -24,5 +24,130 @@ stopword_dict = set(stopwords.words('english'))
 en_model = StanfordCoreNLP(StanfordCoreNLP_path, quiet=True)
 tokenizer = None
 
+def get_long_data(file_path="data/nus/nus_test.json"):
+    """ Load file.jsonl ."""
+    data = {}
+    labels = {}
+    with codecs.open(file_path, 'r', 'utf-8') as f:
+        json_text = f.readlines()
+        for i, line in tqdm(enumerate(json_text), desc="Loading Doc ..."):
+            try:
+                jsonl = json.loads(line)
+                keywords = jsonl['keywords'].lower().split(";")
+                abstract = jsonl['abstract']
+                fulltxt = jsonl['fulltext']
+                doc = ' '.join([abstract, fulltxt])
+                doc = re.sub('\. ', ' . ', doc)
+                doc = re.sub(', ', ' , ', doc)
+                doc = doc.replace('\n', ' ')
+                data[jsonl['name']] = doc
+                labels[jsonl['name']] = keywords
+            except:
+                raise ValueError
+    return data,labels
 
 
+def get_duc2001_data(file_path="data/DUC2001"):
+    pattern = re.compile(r'<TEXT>(.*?)</TEXT>', re.S)
+    data = {}
+    labels = {}
+    for dirname, dirnames, filenames in os.walk(file_path):
+        for fname in filenames:
+            if (fname == "annotations.txt"):
+                # left, right = fname.split('.')
+                infile = os.path.join(dirname, fname)
+                f = open(infile,'rb')
+                text = f.read().decode('utf8')
+                lines = text.splitlines()
+                for line in lines:
+                    left, right = line.split("@")
+                    d = right.split(";")[:-1]
+                    l = left
+                    labels[l] = d
+                f.close()
+            else:
+                infile = os.path.join(dirname, fname)
+                f = open(infile,'rb')
+                text = f.read().decode('utf8')
+                text = re.findall(pattern, text)[0]
+                data[fname] = text
+    return data,labels
+
+def get_inspec_data(file_path="data/Inspec"):
+
+    data={}
+    labels={}
+    for dirname, dirnames, filenames in os.walk(file_path):
+        for fname in filenames:
+            left, right = fname.split('.')
+            if (right == "abstr"):
+                infile = os.path.join(dirname, fname)
+                f=open(infile)
+                text=f.read()
+                text = text.replace("%", '')
+                data[left]=text
+            if (right == "uncontr"):
+                infile = os.path.join(dirname, fname)
+                f=open(infile)
+                text=f.read()
+                text = text.replace("\n\t", '')
+                text=text.replace("\n",' ')
+                label=text.split("; ")
+                labels[left]=label
+    return data,labels
+
+def get_semeval2017_data(data_path="data/SemEval2017/docsutf8",labels_path="data/SemEval2017/keys"):
+
+    data={}
+    labels={}
+    for dirname, dirnames, filenames in os.walk(data_path):
+        for fname in filenames:
+            left, right = fname.split('.')
+            infile = os.path.join(dirname, fname)
+            # f = open(infile, 'rb')
+            # text = f.read().decode('utf8')
+            with codecs.open(infile, "r", "utf-8") as fi:
+                text = fi.read()
+                text = text.replace("%", '')
+            data[left] = text.lower()
+            # f.close()
+    for dirname, dirnames, filenames in os.walk(labels_path):
+        for fname in filenames:
+            left, right = fname.split('.')
+            infile = os.path.join(dirname, fname)
+            f = open(infile, 'rb')
+            text = f.read().decode('utf8')
+            text = text.strip()
+            ls=text.splitlines()
+            labels[left] = ls
+            f.close()
+    return data,labels
+
+def get_short_data(file_path="data/krapivin/kravipin_test.json"):
+    """ Load file.jsonl ."""
+    data = {}
+    labels = {}
+    with codecs.open(file_path, 'r', 'utf-8') as f:
+        json_text = f.readlines()
+        for i, line in tqdm(enumerate(json_text), desc="Loading Doc ..."):
+            try:
+                jsonl = json.loads(line)
+                keywords = jsonl['keywords'].lower().split(";")
+                abstract = jsonl['abstract']
+                doc =abstract
+                doc = re.sub('\. ', ' . ', doc)
+                doc = re.sub(', ', ' , ', doc)
+                doc = doc.replace('\n', ' ')
+                data[i] = doc
+                labels[i] = keywords
+            except:
+                raise ValueError
+    return data,labels
+
+if __name__ == "__main__":
+    data, labels = get_duc2001_data()
+
+    for id in data:
+        print(id)
+        print(data[id])
+        print(labels[id])
